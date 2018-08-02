5e041f8b080915df6f31e030cbef4b83c6d14391
qiukai <513862058@qq.com>
2018-08-02 17:22:16 +0800

    目录结构的梳理

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,46 @@
 module.exports = {
-  title: 'Hello VuePress',
+  title: 'TreasureMap',
   description: 'Just playing around',
   base:'/treasureMap/',
+  themeConfig: {
+    nav: [
+      { text: '前端',link: '/web/' },
+      { text: '全栈',link: '/fullStack/' },
+      { text: '通识',link: '/common/' },
+      { text: '资讯',link: '/info/' },
+      { text: '关于',link: '/about/' },
+      { text: 'GitHub',link: 'https://github.com/GitHubForQiuKai/treasureMap' },
+    ],
+    sidebar: {
+      '/web/': [{
+        title:'前端',
+        collapsable: false,
+        children: [
+          '',
+        ]
+      }],
+      '/fullStack/': [{
+        title:'全栈',
+        collapsable: false,
+        children: [
+          '',
+          'mysql',
+          ]
+      }],
+      '/common/': [{
+        title:'通识',
+        collapsable: false,
+        children: [
+          '',
+          'nginx',
+          'linux',
+          'ssh',
+          'restful',
+          'data-structure',
+          'algorithm',
+          'design-pattern'
+          ]
+      }]
+    }
+  }
 }
